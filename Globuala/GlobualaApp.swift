6fd531d7a1fd55061263ccbffbc96042bb710a79
Jonatan Gonzalez <jonatan_gonzalez@me.com>
2025-05-16 02:48:12 -0300

    Initial Commit

---
+++ Globuala/GlobualaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobualaApp.swift
+//  Globuala
+//
+//  Created by Joni Gonzalez on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GlobualaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
