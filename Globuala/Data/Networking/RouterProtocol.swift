7bc13ae1ffbbc45665b6e9d2038032e002517625
Jonatan Gonzalez <jonatan_gonzalez@me.com>
2025-05-16 21:20:06 -0300

    Se agrega NetworkRouterProtocol

--- Globuala/Data/Networking/RouterProtocol.swift
+++ Globuala/Data/Networking/RouterProtocol.swift
@@ -5,3 +5,17 @@
 //  Created by Joni Gonzalez on 16/05/2025.
 //
 
+import Alamofire
+import Foundation
+
+public typealias NetworkRouterCompletion<GenericObject: Decodable> = (_ response: Result<GenericObject, AFError>) -> ()
+
+protocol NetworkRouterProtocol: AnyObject {
+    associatedtype EndPoint: EndPointType
+    func request<GenericObject: Decodable>(expectedData: GenericObject.Type,
+                                           from route: EndPoint,
+                                           completion: @escaping NetworkRouterCompletion<GenericObject>)
+    
+    func request<GenericObject: Decodable>(expectedData: GenericObject.Type,
+                                           from route: EndPoint) async -> Result<GenericObject, AFError>
+}
